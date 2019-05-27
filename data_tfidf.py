d83de677f7128fe17218cdcd8428550f54f886e6
lamprad <1801210053@pku.edu.cn>
2019-05-27 17:23:13 +0800

    tfidf+word2vec version

--- data_tfidf.py
+++ data_tfidf.py
@@ -4,13 +4,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from scipy.sparse.csr import csr_matrix
 import scipy.sparse as sp
+from gensim.models import Word2Vec
+import re
+
 
 class Dataset:
-    def __init__(self):
-        self.raw_data = self.data_preprocess(config.train_path)
-        corpus = [sentence.lower() for sentence in self.raw_data['review']] + [sentence.lower() for sentence in pd.read_csv(config.test_path)['review'].values]
-        self.corpus = self.convert_(corpus)
-        self.X, self.y, self.test = self.corpus[:config.train_size + config.val_size], np.array([{"Negative": 0, "Positive": 1}[label] for label in self.raw_data["label"]]), self.corpus[-config.test_size - 1:-1]
+    def __init__(self, read_model=True):
+        self.pattern = re.compile(r'(['u'\U0001F300-\U0001F64F'u'\U0001F680-\U0001F6FF'u'\u2600-\u2B55])', re.UNICODE)
+        self.raw_data0, self.raw_data1 = self.data_preprocess(config.train_path)
+        corpus = self.convert_([sentence.lower() for sentence in self.raw_data0['review']] + [sentence.lower() for sentence in pd.read_csv(config.test_path)['review'].values])
+        self.y = np.array([{"Negative": 0, "Positive": 1}[label] for label in self.raw_data0["label"]])
+        tr, te = self.word2vec_data(config.model_path, read_model)
+        self.X = np.concatenate([corpus[:config.train_size + config.val_size], tr], axis=1)
+        self.test = np.concatenate([corpus[-config.test_size - 1:-1], te], axis=1)
 
     def data_preprocess(self, path):
         with open(path) as f:
@@ -30,8 +36,8 @@ class Dataset:
         df = {"ID": [line.split(",", 1)[0] for line in lines[1:]],
               "review": [line.split(",", 1)[1][:-10].lower() for line in lines[1:]],
               "label": [line.split(",", 1)[1][-9:-1] for line in lines[1:]]}
-        return df
-
+        participle = [re.split(self.pattern, review) for review in df["review"]]
+        return df, participle
 
     @staticmethod
     def convert_(corpus):
@@ -44,6 +50,55 @@ class Dataset:
         np_feature_eng = np.concatenate([np_feature_eng, np.vstack(np_feature_eng.std(axis=1))], axis=1)
         np_feature_eng = np.concatenate([np_feature_eng, np.vstack(np.percentile(np_feature_eng, 25, axis=1))], axis=1)
         return np_feature_eng
+
+    @staticmethod
+    def participle(sentences):
+        new_sentences = []
+        for sentence in sentences:
+            new_sentence = []
+            for word in sentence:
+                if len(word) > 1:
+                    new_sentence.extend([char for char in re.split(r'(\w+)', word) if char])
+                elif word:
+                    new_sentence.append(word)
+            new_sentences.append([char for char in new_sentence if char != " "])
+        return new_sentences
+
+    def word2vec_data(self, path, read):
+        train_X = self.participle([sentence for sentence in self.raw_data1])
+        test_X = self.participle(
+            [re.split(self.pattern, sentence) for sentence in pd.read_csv(config.test_path)["review"].values])
+        if not read:
+            model = Word2Vec(train_X + test_X, size=config.embeddingsize, workers=4)
+            model.save(path)
+        else:
+            model = Word2Vec.load(config.model_path)
+        tr = self.padding_sentences([self.convert_data(x, model) for x in train_X], config.padding_size)
+        te = self.padding_sentences([self.convert_data(x, model) for x in test_X], config.padding_size)
+        return tr, te
+
+    @staticmethod
+    def convert_data(sentence, vocab_dic):
+        data = np.zeros((len(sentence), config.embeddingsize))
+        for idx, word in enumerate(sentence):
+            if word in vocab_dic:
+                data[idx, :] = vocab_dic[word]
+        return data
+
+    @staticmethod
+    def padding_sentences(sentences, length):
+        paddles = np.zeros((len(sentences), length * config.embeddingsize))
+        for i, sentence in enumerate(sentences):
+            if length > sentence.shape[0]:
+                paddle = np.concatenate((sentence, np.zeros((length - sentence.shape[0], config.embeddingsize))),
+                                        axis=0)
+                paddles[i, :] = paddle.reshape((1, -1))
+            elif length < sentence.shape[0]:
+                paddle = sentence[:length]
+                paddles[i, :] = paddle.reshape((1, -1))
+            else:
+                paddles[i, :] = sentence.reshape((1, -1))
+        return paddles
 
 
 if __name__ == "__main__":
--- model_classic.py
+++ model_classic.py
@@ -3,30 +3,28 @@ from sklearn.model_selection import StratifiedKFold
 import numpy as np
 from data_tfidf import Dataset
 from sklearn.linear_model import LogisticRegression
-import pandas as pd
-from common import config
-import os
 from sklearn.metrics import roc_auc_score
+from sklearn.naive_bayes import GaussianNB
 
 class model:
 
     def __init__(self, X, y, test, K=5):
-        self.lgbparam = {"objective": "binary", "metric": "auc", "boosting": 'gbdt',
-                         "max_depth": -1,
-                         "num_leaves": 8,
-                         "learning_rate": 0.1,
-                         "bagging_freq": 5,
-                         "bagging_fraction": 0.4,
-                         "feature_fraction": 0.5,
-                         "min_data_in_leaf": 80,
-                         "tree_learner": "serial",
-                         "boost_from_average": "false",
-                         "verbosity": 1}
         self.X, self.y, self.test, self.predict_y = X, y, test, np.zeros(test.shape[0])
         self.K = K
         self.skf = StratifiedKFold(n_splits=K, shuffle=True)
 
     def _lgb(self):
+        lgbparam = {"objective": "binary", "metric": "auc", "boosting": 'gbdt',
+                    "max_depth": -1,
+                    "num_leaves": 30,
+                    "learning_rate": 0.1,
+                    "bagging_freq": 5,
+                    "bagging_fraction": 0.8,
+                    "feature_fraction": 1,
+                    "min_data_in_leaf": 3,
+                    "tree_learner": "serial",
+                    "boost_from_average": "false",
+                    "verbosity": 1}
         for fold, (trn_idx, val_idx) in enumerate(self.skf.split(self.X, self.y)):
             X_train, y_train = self.X[trn_idx, :], self.y[trn_idx]
             X_val, y_val = self.X[val_idx, :], self.y[val_idx]
@@ -35,7 +33,7 @@ class model:
             dval = lgb.Dataset(X_val, label=y_val, reference=dtrain)
 
             print('Start training...')
-            model = lgb.train(self.lgbparam, dtrain, num_boost_round=50000, valid_sets=dval, early_stopping_rounds=500,
+            model = lgb.train(lgbparam, dtrain, num_boost_round=50000, valid_sets=dval, early_stopping_rounds=500,
                               verbose_eval=2500)
 
             print('Training done, exit. Start predicting...')
@@ -47,7 +45,21 @@ class model:
             X_val, y_val = self.X[val_idx, :], self.y[val_idx]
 
             model = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial', C=0.1, max_iter=100000)
-            print('Start training...')
+            print('Start training Logistic regression...')
+            model.fit(X_train, y_train)
+            print("\n**************Validation results****************")
+            print('AUC_avg: {:.3f}'.format(roc_auc_score(y_val, model.predict_proba(X_val)[:, 1])))
+            print("************************************************\n")
+            print('Training done, exit. Start predicting...')
+            self.predict_y += model.predict_proba(self.test)[:, 1] / self.K
+
+    def _nb(self):
+        for fold, (trn_idx, val_idx) in enumerate(self.skf.split(self.X, self.y)):
+            X_train, y_train = self.X[trn_idx, :], self.y[trn_idx]
+            X_val, y_val = self.X[val_idx, :], self.y[val_idx]
+
+            model = GaussianNB()
+            print('Start training naive bayes...')
             model.fit(X_train, y_train)
             print("\n**************Validation results****************")
             print('AUC_avg: {:.3f}'.format(roc_auc_score(y_val, model.predict_proba(X_val)[:, 1])))
